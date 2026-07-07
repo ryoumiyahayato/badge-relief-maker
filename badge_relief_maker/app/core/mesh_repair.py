55754c4dfea19271b331a491b1a63057f5bd692a
ryouma <junjieduan8@gmail.com>
2026-07-06 23:04:27 -0700

    Add mesh repair placeholders

---
+++ badge_relief_maker/app/core/mesh_repair.py
@@ -0,0 +1,11 @@
+"""Mesh repair placeholder utilities."""
+
+
+def face_count(faces):
+    """Return the number of mesh faces."""
+    return int(len(faces))
+
+
+def remove_duplicate_vertices(vertices, faces):
+    """Placeholder for vertex cleanup."""
+    return vertices, faces
